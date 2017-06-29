6c3b27e5cea45480708c84c6517ce8228fff39e4
KeziahMoselle <keziahmoselle@protonmail.com>
2017-06-29 20:32:12 +0200

    rewriting

--- upload.php
+++ upload.php
@@ -1,119 +1,95 @@
 <?php
 session_start();
 
+require 'libs/db.php';
+require 'libs/osu_api.php';
+
 if (isset($_POST['upload']))
 {
-    $token = htmlspecialchars($_GET['token']);
-    if (isset($_GET['token']) AND $token == $_SESSION['token'])
+  if (isset($_POST['beatmap_url'],$_POST['player_username'],$_FILES['file']))
+  {
+    if (isset($_POST['visibility']))
     {
+        $visibility = "private";
+    }
+    else
+    {
+        $visibility = "public";
+    }
 
-      if (isset($_POST['beatmap_url']))
-      {
-        if (isset($_POST['player_username']))
+    // Beatmap
+    $beatmap_url = htmlspecialchars($_POST['beatmap_url']); // POST DATA
+    preg_match('^https:\/\/osu\.ppy\.sh\/b\/(\d*)^', $beatmap_url, $matches); // REGEX
+    $beatmap_id = $matches[1]; // FETCH ID
+    $beatmap = get_beatmaps($key,$beatmap_id); // FETCH BM INFO
+    // Beatmap info
+    $artist = $beatmap[0]["artist"];
+    $title = $beatmap[0]["title"];
+    $version = $beatmap[0]["version"];
+    $creator = $beatmap[0]["creator"];
+    $mode = $beatmap[0]["mode"];
+    $difficultyrating = round($beatmap[0]["difficultyrating"], 2);
+    $beatmapset_id = $beatmap[0]["beatmapset_id"];
+    // Player
+    $player_username = htmlspecialchars($_POST['player_username']); // POST DATA
+    $get_user = get_user($key,$player_username); // FETCH USER INFO
+    $player = $get_user[0]["username"];
+    $player_id = $get_user[0]["user_id"];
+    $player_url = 'https://osu.ppy.sh/u/'.$player_id;
+    $player_rank = $get_user[0]["pp_rank"];
+    $country = $get_user[0]["country"];
+    // Infos
+    $uploader = $_SESSION['username'];
+    // Traitement du fichier
+    $file = $_FILES['file'];
+    $fileName = $_FILES['file']['name'];
+    $fileTmpName = $_FILES['file']['tmp_name'];
+    $fileSize = $_FILES['file']['size'];
+    $fileError = $_FILES['file']['error'];
+    $fileType = $_FILES['file']['type'];
+    $fileExt = explode('.', $fileName);
+    $fileActualExt = strtolower(end($fileExt));
+    if ($fileActualExt == 'osr')
+    {
+        if ($fileError === 0)
         {
-            if (isset($_POST['visibility']))
+            if ($fileSize < 10000000)
             {
-                $visibility = "private";
+                require 'libs/db.php';
+                $req = $db->prepare('SELECT replays FROM users WHERE id=?');
+                $req->execute(array($_SESSION['id']));
+                $results = $req->fetch();
+                $ReplayCounter = $results["replays"] + 1;
+                $updateReplayCounter = $db->prepare("UPDATE users SET replays=? WHERE id=?");
+                $updateReplayCounter->execute(array($ReplayCounter,$_SESSION['id']));
+                $fileNameNew = "$player : $artist - $title [$version] ($uploader($ReplayCounter)).$fileActualExt";
+                $fileDestination = 'uploads/'.$fileNameNew;
+                move_uploaded_file($fileTmpName, $fileDestination);
+                // Insert DB
+                $newReplay = $db->prepare("INSERT INTO replays(visibility,replay_url,artist,title,version,creator,mode,difficultyrating,beatmap_id,beatmap_url,beatmapset_id,player,player_rank,country,player_id,player_url,uploader) VALUES (?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?)");
+                $newReplay->execute(array($visibility,$fileNameNew,$artist,$title,$version,$creator,$mode,$difficultyrating,$beatmap_id,$beatmap_url,$beatmapset_id,$player,$player_rank,$country,$player_id,$player_url,$uploader));
+                $notif = "Replay uploaded.";
             }
             else
             {
-                $visibility = "public";
-            }
-
-            require 'libs/osu_api.php';
-
-            // Beatmap
-            $beatmap_url = htmlspecialchars($_POST['beatmap_url']); // POST DATA
-            preg_match('^https:\/\/osu\.ppy\.sh\/b\/(\d*)^', $beatmap_url, $matches); // REGEX
-            $beatmap_id = $matches[1]; // FETCH ID
-            $beatmap = get_beatmaps($key,$beatmap_id); // FETCH BM INFO
-            // Beatmap info
-            $artist = $beatmap[0]["artist"];
-            $title = $beatmap[0]["title"];
-            $version = $beatmap[0]["version"];
-            $creator = $beatmap[0]["creator"];
-            $mode = $beatmap[0]["mode"];
-            $difficultyrating = round($beatmap[0]["difficultyrating"], 2);
-            $beatmapset_id = $beatmap[0]["beatmapset_id"];
-
-            // Player
-            $player_username = htmlspecialchars($_POST['player_username']); // POST DATA
-            $get_user = get_user($key,$player_username); // FETCH USER INFO
-            $player = $get_user[0]["username"];
-            $player_id = $get_user[0]["user_id"];
-            $player_url = 'https://osu.ppy.sh/u/'.$player_id;
-            $player_rank = $get_user[0]["pp_rank"];
-            $country = $get_user[0]["country"];
-            // Infos
-            $uploader = $_SESSION['username'];
-
-
-            // Traitement du fichier
-            $file = $_FILES['file'];
-            $fileName = $_FILES['file']['name'];
-            $fileTmpName = $_FILES['file']['tmp_name'];
-            $fileSize = $_FILES['file']['size'];
-            $fileError = $_FILES['file']['error'];
-            $fileType = $_FILES['file']['type'];
-
-            $fileExt = explode('.', $fileName);
-            $fileActualExt = strtolower(end($fileExt));
-
-
-            if ($fileActualExt == 'osr')
-            {
-                if ($fileError === 0)
-                {
-                    if ($fileSize < 10000000)
-                    {
-                        require 'libs/db.php';
-
-                        $req = $db->prepare('SELECT replays FROM users WHERE id=?');
-                        $req->execute(array($_SESSION['id']));
-                        $results = $req->fetch();
-                        $ReplayCounter = $results["replays"] + 1;
-                        $updateReplayCounter = $db->prepare("UPDATE users SET replays=? WHERE id=?");
-                        $updateReplayCounter->execute(array($ReplayCounter,$_SESSION['id']));
-
-                        $fileNameNew = "$player : $artist - $title [$version] ($uploader($ReplayCounter)).$fileActualExt";
-                        $fileDestination = 'uploads/'.$fileNameNew;
-                        move_uploaded_file($fileTmpName, $fileDestination);
-                        // Insert DB
-                        $newReplay = $db->prepare("INSERT INTO replays(visibility,replay_url,artist,title,version,creator,mode,difficultyrating,beatmap_id,beatmap_url,beatmapset_id,player,player_rank,country,player_id,player_url,uploader) VALUES (?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?)");
-                        $newReplay->execute(array($visibility,$fileNameNew,$artist,$title,$version,$creator,$mode,$difficultyrating,$beatmap_id,$beatmap_url,$beatmapset_id,$player,$player_rank,$country,$player_id,$player_url,$uploader));
-
-                        $notif = "Replay uploaded.";
-                    }
-                    else
-                    {
-                        $notif = "Your file is too big.<br/>";
-                    }
-                }
-                else
-                {
-                    $notif = "There was an error uploading your file.<br/>";
-                }
-            }
-            else
-            {
-                $notif = "Your file need to be an .osr<br/>";
+                $notif = "Your file is too big.<br/>";
             }
         }
         else
         {
-          $notif .= "Player username is empty.<br/>";
+            $notif = "There was an error uploading your file.<br/>";
         }
-      }
-      else
-      {
-        $notif .= "Beatmap URL is empty.<br/>";
-      }
-
     }
     else
     {
-        die('Token error.');
+        $notif = "Your file need to be an .osr<br/>";
     }
+
+  }
+  else
+  {
+    die('Input missing');
+  }
 }
 
 ?>
@@ -132,6 +108,16 @@ if (isset($_POST['upload']))
 
         <?php require 'templates/header.php'; ?>
 
+        <nav>
+            <div class="nav-wrapper grey darken-3">
+                <div class="col s12 center-align">
+                    <span class="white-text">
+                      <?php if(isset($notif)){ echo $notif; } ?>
+                    </span>
+                </div>
+            </div>
+        </nav>
+
         <main>
 
             <div class="row">
@@ -143,18 +129,14 @@ if (isset($_POST['upload']))
                           <h1 class="center-align">Upload</h1>
                         </div>
                         <div class="card-content">
-                          <?php if(isset($notif)): ?>
-                              <div class="center-align">
-                                  <span class="black-text"><?= $notif ?></span>
-                              </div>
-                          <?php endif ?>
+
                             <form class="row" action="upload.php?token=<?=$_SESSION['token']?>" method="post" enctype="multipart/form-data">
                                 <div class="input-field col s12 l6 offset-l3">
-                                  <input name="beatmap_url" id="beatmap_url" type="text" placeholder="https://osu.ppy.sh/b/00000" required>
+                                  <input name="beatmap_url" id="beatmap_url" type="text" placeholder="https://osu.ppy.sh/b/00000" value="https://osu.ppy.sh/b/787430?m=0" required>
                                   <label for="beatmap_url">Beatmap URL</label>
                                 </div>
                                 <div class="input-field col s12 l6 offset-l3">
-                                  <input name="player_username" id="player_username" type="text" placeholder="Abcdef" required>
+                                  <input name="player_username" id="player_username" type="text" placeholder="Abcdef" value="Nemesis-" required>
                                   <label for="player_username">Player username</label>
                                 </div>
                                 <div class="file-field input-field col s12 m11 offset-m1">
@@ -170,11 +152,15 @@ if (isset($_POST['upload']))
                                     <div class="switch">
                                         <label>
                                             Public
-                                            <input name="visibility" type="checkbox">
+                                            <input name="visibility" type="checkbox" checked>
                                             <span class="lever"></span>
                                             Private
                                         </label>
                                     </div>
+                                </div>
+                                <div class="input-field col s12 l6 offset-l3">
+                                  <input name="youtube_url" id="youtube_url" type="text" placeholder="https://www.youtube.com/watch?v=abcdef0123">
+                                  <label for="youtube_url">Youtube URL</label>
                                 </div>
                         </div>
                         <div class="card-action center">
