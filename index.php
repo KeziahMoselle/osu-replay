49850b8bd1397a88c864b3236b0ff131f936c6e7
KeziahMoselle <keziahmoselle@protonmail.com>
2017-07-01 13:45:37 +0200

    Fix

--- index.php
+++ index.php
@@ -38,12 +38,10 @@ $replays->execute(array("public"));
 
               <div class="col s12 center">
                 <ul class="pagination">
-                  <li class="waves-effect"><a href="#!"><i class="material-icons">chevron_left</i></a></li>
+                  <li class="disabled waves-effect"><a href="#!"><i class="material-icons">chevron_left</i></a></li>
                   <li class="waves-effect active"><a href="#!">1</a></li>
                   <li class="waves-effect"><a href="#!">2</a></li>
                   <li class="waves-effect"><a href="#!">3</a></li>
-                  <li class="waves-effect"><a href="#!">4</a></li>
-                  <li class="waves-effect"><a href="#!">5</a></li>
                   <li class="waves-effect"><a href="#!"><i class="material-icons">chevron_right</i></a></li>
                 </ul>
               </div>
