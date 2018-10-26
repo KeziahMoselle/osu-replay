96c9cceb7db5fec6202944cdc2f81ccbca65bfed
Nemesis <keziah.moselle@gmail.com>
2018-10-26 20:58:05 +0200

    Fix security issue

--- libs/db.php
+++ libs/db.php
@@ -5,7 +5,7 @@ require('config.php');
 $db = new PDO("mysql:host=localhost;dbname=osureplay", $config['DB_USER'], $config['DB_PASSWORD']);
 
 if (isset($_SESSION['auth']) && $_SESSION['auth'] == 1) {
-    $req = $db->prepare('SELECT * FROM users WHERE id = ?');
+    $req = $db->prepare('SELECT id, username, user_id, avatar_link, replays FROM users WHERE id = ?');
     $req->execute(array($_SESSION['id']));
     $user = $req->fetch();
 } else {
